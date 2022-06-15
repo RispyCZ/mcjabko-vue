013792e49912b20e992253adacbab91f590bcbd9
RispyCZ <petadj88@gmail.com>
2022-06-15 22:29:42 +0200

    Clean up imports

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router"
-import Footer from "./components/Footer.vue"
-import Navbar from "./components/Navbar.vue"
+import Footer from "@/components/Footer.vue"
+import Navbar from "@/components/Navbar.vue"
 </script>
 
 <template>
